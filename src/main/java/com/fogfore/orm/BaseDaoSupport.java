ace7b5de98d7a796609c0c1aa17f79a1375bb3c1
fogfore <38969741+fogfore@users.noreply.github.com>
2019-05-10 18:00:07 +0800

    Update BaseDaoSupport.java

--- src/main/java/com/fogfore/orm/BaseDaoSupport.java
+++ src/main/java/com/fogfore/orm/BaseDaoSupport.java
@@ -1,7 +1,77 @@
 package com.fogfore.orm;
 
+import org.apache.commons.lang3.ObjectUtils;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
 import java.io.Serializable;
+import java.util.List;
+import java.util.Map;
 
-public class BaseDaoSupport<T extends Serializable> {
+public class BaseDaoSupport<T extends Serializable, PK extends Serializable> {
+    private EntityOperation<T> entityOperation;
+    private JdbcTemplate jdbcTemplate;
+    private DataSource dataSource;
 
+    @SuppressWarnings("unchecked")
+    public BaseDaoSupport() {
+        try {
+            Class<T> entityClass = GenericsUtils.getSuperClassGenericType(getClass());
+            this.entityOperation = new EntityOperation<T>(entityClass);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Map<String, Object> parse(T t) {
+        if (ObjectUtils.isEmpty(t)) {
+            return null;
+        }
+        return entityOperation.parse(t);
+    }
+
+    public String getTableName() {
+        return this.entityOperation.getTableName();
+    }
+
+    public T select(PK id) {
+        return null;
+    }
+
+    public List<T> selectAll() {
+        return null;
+    }
+
+    public boolean insert(T t) {
+        return doInsert(t);
+    }
+
+    public PK insertAndReturnId(T t) {
+        return null;
+    }
+
+    public boolean insertAll(List<T> list) {
+        return false;
+    }
+
+    public boolean update(T t) {
+        return false;
+    }
+
+    public boolean updateAll(List<T> list) {
+        return false;
+    }
+
+    public boolean delete(T t) {
+        return false;
+    }
+
+    public boolean deleteAll(List<T> list) {
+        return false;
+    }
+
+    private boolean doInsert(T t) {
+        Map<String, Object> parse = entityOperation.parse(t);
+        return false;
+    }
 }
